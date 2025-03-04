3bd6236d54249ba76a39585674fe57f15061f6a2
AndreiTorres-0417 <thesecondhero0417@gmail.com>
2025-03-04 14:12:57 +0000

    Fixed Typo for video game section

--- my-profile-site/src/components/ui/AccordionComponent.vue
+++ my-profile-site/src/components/ui/AccordionComponent.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue'
 
 const defaultValue = ref('item-1')
 const accordionItems = [
-  { value: 'item-1', title: 'What are you favorite video games?', content: 'My facorite games at the moment are Lethal Company, Minecraft, Phasmophobia, Terraria, and Tetris.' },
+  { value: 'item-1', title: 'What are you favorite video games?', content: 'My favorite games at the moment are Lethal Company, Minecraft, Phasmophobia, Terraria, and Tetris.' },
   { value: 'item-2', title: 'Dogs or Cats?', content: 'I love both! But I have a softer spot for cats' },
   { value: 'item-3', title: 'Favorite drink?', content: 'I love cola flavored soda. There are times I drink too much so I better keep an eye on that' }
 ]
