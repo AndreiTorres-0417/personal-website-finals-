2282debd6ef0b4da31f33b6196b3262bfcf91033
AndreiTorres-0417 <thesecondhero0417@gmail.com>
2025-03-04 12:33:49 +0000

    revert

--- my-profile-site/vue.config.js
+++ my-profile-site/vue.config.js
@@ -2,3 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    client: {
+      webSocketURL: {
+        hostname: 'localhost',
+        pathname: '/ws',
+        port: 8080,
+        protocol: 'ws'
+      }
+    }
+  }
+};
